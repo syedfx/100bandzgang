52075cb22c765fa712b634a292e36e8241d5a7b8
tauhid33 <tauhidahmed1090@gmail.com>
2022-06-10 11:04:29 -0400

    40 test cases done

--- src/test/java/apps/rahulshettyacademy/PythonCourseInfo.java
+++ src/test/java/apps/rahulshettyacademy/PythonCourseInfo.java
@@ -4,11 +4,12 @@ import base.CommonAPI;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class SearchTest extends CommonAPI {
+public class PythonCourseInfo extends CommonAPI {
     @Test
-    public void Search(){
+    public void courseInfoPython(){
+
         driver.findElement(By.cssSelector("a[href='https://courses.rahulshettyacademy.com/courses'")).click();
-        driver.findElement(By.id("search-courses")).sendKeys("Java");
-        driver.findElement(By.id("search-course-button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[2]/div[14]/div/div[1]/a/div/div[1]/img")).click();
+
     }
 }
